814c2a4c4875f32f505bcfd759c8485d327ae67e
Micheal Bear <jacquelinecadybear@gmail.com>
2025-03-08 23:47:39 -0800

    mostly finished exersise 3.1 but still has a weird err where it takes random chars from memory that I could revisit

--- echo-3.1.c
+++ echo-3.1.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 
 int main(){
 	char curr, name[64], age[10];
-	int i = 0;
-	curr = getcharacter();	
-		\\get line
+	int i, dAge;
+	i = 0;
+	curr = getchar();	
+		//get line
 	while (curr != '\n'){
 		name[i] = curr;
-		curr = getcharacter();
-
-	
-	
+		curr = getchar();
+		i += 1;	
 	}
+	curr = getchar();
+	i = 0;
+	while (curr != '\n'){
+		age[i] = curr;
+		curr = getchar();
+		i += 1;
+	}
+	dAge = atoi(age)*7;
+	
+	printf("name:%.10s\tname:%d", name, dAge);
 }
