f1ff15feef1ecae48f0f29f4fd3831d5043520cf
Micheal Bear <jacquelinecadybear@gmail.com>
2025-03-10 12:33:03 -0700

    chapter 2 exersise 1

---
+++ leapYear.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*leap year happens on multiples of 4, 
+ * except years divisible byt 100 but not 400 */
+
+
+int main(){
+	char year[4], curr;
+	int yr, i = 0;
+	
+	printf("enter a year: ");	
+	while ((curr = getchar()) != '\n'){
+		year[i] = curr;
+		i ++;
+		//curr = getchar();
+	}
+	yr = atoi(year);
+	/*isleap = (yr % 4 == 0 &&!(yr % 400 != 0 &&   ))*/
+	if (yr % 4 == 0){
+		//printf("hi");
+		if (yr % 100 == 0 && yr % 400 != 0) printf("%d is not a leap year", yr);
+		else printf("%d is a leap year", yr);	
+	
+	}
+	else printf("%d is not a leap year", yr);
+	
+	return EXIT_SUCCESS;
+}
