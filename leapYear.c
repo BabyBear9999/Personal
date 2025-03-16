44c3cf8490fe8c9f99a056bc7ad02b9a83a9328f
BabyBear9999 <Jaquelinecadybear@gmail.com>
2025-03-15 23:08:36 -0700

    added newline at end of output

--- leapYear.c
+++ leapYear.c
@@ -19,11 +19,11 @@ int main(){
 	/*isleap = (yr % 4 == 0 &&!(yr % 400 != 0 &&   ))*/
 	if (yr % 4 == 0){
 		//printf("hi");
-		if (yr % 100 == 0 && yr % 400 != 0) printf("%d is not a leap year", yr);
-		else printf("%d is a leap year", yr);	
+		if (yr % 100 == 0 && yr % 400 != 0) printf("%d is not a leap year\n", yr);
+		else printf("%d is a leap year\n", yr);	
 	
 	}
-	else printf("%d is not a leap year", yr);
+	else printf("%d is not a leap year \n", yr);
 	
 	return EXIT_SUCCESS;
 }
